5711ee8efcd8e34a211f9cab14e1afe6f3a7401c
kumard3 <kumardeepanshu157200@gmail.com>
2022-02-14 20:04:59 +0530

    changed to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import "./styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
