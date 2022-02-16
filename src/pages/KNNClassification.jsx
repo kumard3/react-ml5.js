7a3bcf63a0e24bc74388e8cb8adb2fb6d08a7459
kumard3 <kumardeepanshu157200@gmail.com>
2022-02-16 14:19:47 +0530

    KNNClassification

--- src/pages/KNNClassification.jsx
+++ src/pages/KNNClassification.jsx
@@ -1,14 +1,13 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import ml5 from "ml5";
 import p5 from "p5";
 
 export default function KNNClassification() {
   const app = useRef();
-  const [modeldata, setModelData] = useState([]);
-  let [video, setvideo] = useState();
 
+  let video;
   let mobilenet;
-  console.log(mobilenet);
+  let knn;
   const sketch = (p) => {
     function modelReady() {
       console.log("Model is ready!!!");
@@ -20,15 +19,34 @@ export default function KNNClassification() {
       video.hide();
       p.background(0);
       mobilenet = ml5.featureExtractor("MobileNet", modelReady);
+      knn = ml5.KNNClassifier();
     };
     p.draw = () => {
       p.image(video, 0, 0);
     };
-
+    function gotResults(error, results) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(results);
+      }
+    }
     p.mousePressed = () => {
-      console.log("first");
+      if (knn.getNumLabels() > 0) {
+        const logits = mobilenet.infer(video);
+        knn.classify(logits, gotResults);
+      }
+    };
+
+    p.keyPressed = () => {
       const logits = mobilenet.infer(video);
-      console.log(logits);
+      if (p.key === "l") {
+        knn.addExample(logits, "left");
+        console.log("left");
+      } else if (p.key === "r") {
+        knn.addExample(logits, "right");
+        console.log("right");
+      }
     };
   };
   useEffect(() => {
@@ -38,7 +56,7 @@ export default function KNNClassification() {
       newp5.remove();
     };
   }, []);
-
+  // console.log(knn)
   return (
     <div className="flex justify-center items-center">
       <div ref={app} />
